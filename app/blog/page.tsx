ffbf0d4082aed43c241dbca111d1c64f27071dcb
Guillaume Slevan-Tremblay <guillaume.slevant@gmail.com>
2026-01-18 01:31:26 -0800

    Add content to research tab

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,7 +5,7 @@ export const revalidate = 60;
 
 export const metadata = {
   title: "Blog – Guillaume Slevan-Tremblay",
-  description: "Latest writing and research updates from Guillaume Slevan-Tremblay.",
+  description: "Latest thoughts, research and writing from Guillaume Slevan-Tremblay.",
 };
 
 export default async function BlogPage() {
